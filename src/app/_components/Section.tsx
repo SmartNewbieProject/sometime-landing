303ca1cb803c2755dfb82fd5683ca0a1b02f31a6
mayrang <pkss0626@naver.com>
2025-07-27 18:52:03 +0900

    Fix: 높이 관련 설정

--- src/app/_components/Section.tsx
+++ src/app/_components/Section.tsx
@@ -5,14 +5,7 @@ import { useEffect } from "react";
 export default function Section({ children }: { children: React.ReactNode }) {
   return (
     <div className="section fp-responsive fp-auto-height-responsive flex font-pretendard justify-center max-w-[440px] mx-auto items-center w-full  bg-white section">
-      <div
-        className="w-full"
-        style={{
-          height: "calc(var(--vh, 1vh) * 100)",
-        }}
-      >
-        {children}
-      </div>
+      <div className="w-full">{children}</div>
     </div>
   );
 }
