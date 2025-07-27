d12854b60a112f146ad6cd9de537841f6e2fe043
mayrang <pkss0626@naver.com>
2025-07-27 19:00:20 +0900

    Fix: 아 이거 높이때문에 계속 스트레스네

--- src/app/_components/Section.tsx
+++ src/app/_components/Section.tsx
@@ -4,8 +4,8 @@ import { useEffect } from "react";
 
 export default function Section({ children }: { children: React.ReactNode }) {
   return (
-    <div className="section fp-responsive fp-auto-height-responsive flex font-pretendard justify-center max-w-[440px] mx-auto items-center w-full  bg-white section">
-      <div className="w-full">{children}</div>
+    <div className="section fp-responsive fp-auto-height-responsive flex font-pretendard justify-center max-w-[440px] h-[100dvh] mx-auto items-center w-full  bg-white section">
+      <div className="w-full h-full">{children}</div>
     </div>
   );
 }
