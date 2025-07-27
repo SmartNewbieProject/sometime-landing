e038f0f9c6bd23ec5330fc69ed249fa5533c227d
mayrang <pkss0626@naver.com>
2025-07-27 18:49:30 +0900

    Fix: 반응형 추가

--- src/app/_components/Section.tsx
+++ src/app/_components/Section.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 export default function Section({ children }: { children: React.ReactNode }) {
   return (
-    <div className="section flex font-pretendard justify-center max-w-[440px] mx-auto items-center w-full  bg-white section">
+    <div className="section fp-responsive fp-auto-height-responsive flex font-pretendard justify-center max-w-[440px] mx-auto items-center w-full  bg-white section">
       <div
         className="w-full"
         style={{
