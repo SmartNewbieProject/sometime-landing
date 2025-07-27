bafcd68bf003f726d039a918fcec39fcd5f16648
mayrang <pkss0626@naver.com>
2025-07-27 18:19:34 +0900

    Fix: 화면 높이 지정

--- src/app/_components/Section.tsx
+++ src/app/_components/Section.tsx
@@ -1,9 +1,28 @@
-import FloatingButton from "./FloatingButton";
+"use client";
+
+import { useEffect } from "react";
 
 export default function Section({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const setVH = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
+    setVH();
+    window.addEventListener("resize", setVH);
+    return () => window.removeEventListener("resize", setVH);
+  }, []);
+
   return (
     <div className="section flex font-pretendard justify-center max-w-[440px] mx-auto items-center w-full min-h-screen bg-white section">
-      <div className="w-full h-screen ">{children}</div>
+      <div
+        className="w-full"
+        style={{
+          height: "calc(var(--vh, 1vh) * 100)",
+        }}
+      >
+        {children}
+      </div>
     </div>
   );
 }
