0ddd864b29491fd6e61de1c9880790e986ce9ea7
mayrang <pkss0626@naver.com>
2025-07-27 18:23:42 +0900

    Fix: h-screen 빼기

--- src/app/_components/Section.tsx
+++ src/app/_components/Section.tsx
@@ -3,18 +3,8 @@
 import { useEffect } from "react";
 
 export default function Section({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    const setVH = () => {
-      const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty("--vh", `${vh}px`);
-    };
-    setVH();
-    window.addEventListener("resize", setVH);
-    return () => window.removeEventListener("resize", setVH);
-  }, []);
-
   return (
-    <div className="section flex font-pretendard justify-center max-w-[440px] mx-auto items-center w-full min-h-screen bg-white section">
+    <div className="section flex font-pretendard justify-center max-w-[440px] mx-auto items-center w-full  bg-white section">
       <div
         className="w-full"
         style={{
