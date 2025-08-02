25751ea85a52b2a50e529a7f2480ec4ea116f920
mayrang <pkss0626@naver.com>
2025-08-02 19:42:09 +0900

    Feat: 메타태그 옵션 a 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,31 +17,30 @@ const wnatedSans = localFont({
 });
 export const metadata = {
   title: {
-    default: "썸타임 - 지역 기반 대학생 AI 맞춤 소개팅 서비스",
+    default: "썸타임 - 동네 대학생 연애 트렌드의 중심, 우리 지역 캠퍼스 소개팅",
     template: "썸타임",
   },
   description:
-    "내 주변 대학교 친구들과 안전한 만남! 인공지능이 나에게 딱 맞는 대학생 친구를 찾아드립니다. 100% 대학생 인증, 지역 기반 맞춤 소개팅 서비스.",
+    "지금 우리 동네 대학생들 사이에서 가장 인기있는 연애 트렌드! 같은 지역 캠퍼스 친구들과의 안전하고 검증된 특별한 만남이 썸타임에서 시작됩니다.",
   keywords: [
-    "대학생 소개팅",
-    "지역별 소개팅",
-    "AI 매칭",
-    "캠퍼스 소개팅",
-    "대학생 미팅",
-    "대학교 연애",
+    "동네 대학생 연애 트렌드",
+    "지역 대학생 연애 중심",
+    "캠퍼스 연애 트렌드",
+    "우리 지역 대학생 소개팅",
+    "동네 대학교 연애 문화, 썸타임",
   ],
   openGraph: {
-    title: "썸타임 - 지역 기반 대학생 AI 맞춤 소개팅",
+    title: "썸타임 - 동네 대학생 연애 트렌드의 중심, 우리 지역 캠퍼스 소개팅",
     description:
-      "내 주변 대학교 친구들과 안전한 만남! 인공지능이 나에게 딱 맞는 대학생 친구를 찾아드립니다.",
+      "지금 우리 동네 대학생들 사이에서 가장 인기있는 연애 트렌드! 같은 지역 캠퍼스 친구들과의 안전하고 검증된 특별한 만남을 시작하세요.",
     url: "https://some-in-univ.com/",
     siteName: "썸타임",
     images: [
       {
-        url: "https://some-in-univ.com/preview_title.png",
-        width: 1200,
-        height: 630,
-        alt: "썸타임 - 대학생 소개팅 서비스",
+        url: "https://info.some-in-univ.com/images/logo.png",
+        width: 140,
+        height: 140,
+        alt: "썸타임 - 동네 대학생 연애 트렌드 중심 캠퍼스 소개팅 서비스",
       },
     ],
     locale: "ko_KR",
@@ -67,20 +66,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta
-          name="google-site-verification"
-          content="zvx72K3s6z5oR8P23QUXqtYP-CjGML5PTs3-B-a058Q"
-        />
-        <meta
-          name="google-site-verification"
-          content="ecsgQ3pQGNVL8u-cOqqvNC4jrddHCtnSJR1QhfBex5E"
-        />
+        <meta name="google-site-verification" content="zvx72K3s6z5oR8P23QUXqtYP-CjGML5PTs3-B-a058Q" />
+        <meta name="google-site-verification" content="ecsgQ3pQGNVL8u-cOqqvNC4jrddHCtnSJR1QhfBex5E" />
+        <meta name="google-site-verification" content="mMaF6ssWvuNzoDifNcxgtIBL4PsHrTdsTJdsRRGBOy4" />
       </head>
-      <body
-        className={`${pretendard.variable} ${wnatedSans.variable} antialiased relative`}
-      >
-        {children}
-      </body>
+      <body className={`${pretendard.variable} ${wnatedSans.variable} antialiased relative`}>{children}</body>
       <GoogleAnalytics gaId="G-VZ7HHRS8QF" />
     </html>
   );
