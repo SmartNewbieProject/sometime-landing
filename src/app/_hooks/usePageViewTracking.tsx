a07f9b225f61b76ddbb6ad3997dd26fc7edc3a9d
mayrang <pkss0626@naver.com>
2025-08-04 15:48:20 +0900

    Fix: 체류시간 rawSeconds 추가

--- src/app/_hooks/usePageViewTracking.tsx
+++ src/app/_hooks/usePageViewTracking.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import { usePathname } from "next/navigation";
 import { track } from "@amplitude/analytics-browser";
 
 export default function usePageViewTracking() {
